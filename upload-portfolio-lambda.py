13960b4f05d568fe78ddf3e44803f655cb901d6f
Chris Dodds <chris.dodds@symphonytalent.com>
2017-09-28 12:42:48 -0500

    updated deploy lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,17 +4,19 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-site_bucket = s3.Bucket('chrisdodds.info')
-build_bucket = s3.Bucket('build.chrisdodds.info')
-
-site_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfolio_build.zip', site_zip)
-
-with zipfile.ZipFile(site_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        site_bucket.upload_fileobj(obj, nm,
-                                   ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        site_bucket.Object(nm).Acl().put(ACL='public-read')
+    site_bucket = s3.Bucket('chrisdodds.info')
+    build_bucket = s3.Bucket('build.chrisdodds.info')
+    
+    site_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfolio_build.zip', site_zip)
+    
+    with zipfile.ZipFile(site_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            site_bucket.upload_fileobj(obj, nm,
+                                       ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            site_bucket.Object(nm).Acl().put(ACL='public-read')
+    return 'Job Done'
